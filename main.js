c9b05871761949b185dce7bf9862828b631479ea
devenock <devenock@localhost.localdomain>
2020-09-29 13:51:12 -0400

    Working on the responsiveness

---
+++ main.js
@@ -0,0 +1,9 @@
+const myHead = document.querySelectorAll('.head');
+
+const myContent = document.querySelectorAll('.content');
+
+myHead.addEventListener('click', function() {
+    if (true) {
+        myContent.style.display = 'block';
+    }
+});
